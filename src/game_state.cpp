3443eba2a69f253fbd6661c8af041bb067cc6670
Matthew Ross <kiswa.com@gmail.com>
2015-05-04 13:52:31 -0400

    Added fixed-timestep gameloop with FPS/UPS display.

--- src/game_state.cpp
+++ src/game_state.cpp
@@ -1,3 +1,4 @@
+#include <sstream>
 #include "game_state.hpp"
 #include "pause_state.hpp"
 
@@ -19,10 +20,16 @@ namespace kg {
         _text.setColor(sf::Color::Black);
         _text.setPosition(4, _window->getSize().y - 30);
 
+        _stats.setFont(_font);
+        _stats.setCharacterSize(12);
+        _stats.setColor(sf::Color::Black);
+        _stats.setPosition(4, 4);
+
         _texture.loadFromFile("assets/test.png");
         _texture.setSmooth(true);
 
         _sprite.setTexture(_texture);
+        _sprite.setPosition(30, 30);
         _sprite.scale(0.5, 0.5);
     }
 
@@ -31,10 +38,37 @@ namespace kg {
     }
 
     void GameState::mainLoop() {
+        float newTime, frameTime, interpolation;
+
+        float currentTime = _clock.getElapsedTime().asSeconds();
+        float accumulator = 0.0f;
+
+        _upsClock.restart();
+        _fpsClock.restart();
+
         while (_window->isOpen()) {
-            handleInput();
-            update(0);
-            render(0);
+            newTime = _clock.getElapsedTime().asSeconds();
+            frameTime = newTime - currentTime;
+
+            if (frameTime > 0.25f) {
+                frameTime = 0.25f;
+            }
+
+            currentTime = newTime;
+            accumulator += frameTime;
+
+            while (accumulator >= dt) {
+                handleInput();
+                update(dt);
+
+                accumulator -= dt;
+            }
+
+            // Don't make render call if window is closed.
+            if (_window->isOpen()) {
+                interpolation = accumulator / dt;
+                render(interpolation);
+            }
         }
     }
 
@@ -57,26 +91,54 @@ namespace kg {
         float x = 0;
         float y = 0;
 
+        //****************************
+        // This is just to display UPS
+        _updates++;
+        float time = _upsClock.getElapsedTime().asSeconds();
+        if (time >= 1.0f) {
+            _ups = _updates;
+
+            _upsClock.restart();
+            _updates = 0;
+        }
+        //****************************
+
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            --x;
+            x -= 2;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            ++x;
+            x += 2;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            --y;
+            y -= 2;
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            ++y;
+            y += 2;
         }
 
         _sprite.move(x, y);
     }
 
     void GameState::render(float dt) {
+        //****************************
+        // This is just to display FPS
+        _frames++;
+        float time = _fpsClock.getElapsedTime().asSeconds();
+        if (time >= 1.0f) {
+            _fps = _frames;
+
+            _fpsClock.restart();
+            _frames = 0;
+        }
+        std::stringstream ss;
+        ss << "UPS: " << _ups << " FPS: " << _fps << " (approx.)";
+        _stats.setString(ss.str());
+        //****************************
+
         _window->clear(sf::Color::Red);
         _window->draw(_sprite);
         _window->draw(_text);
+        _window->draw(_stats); // Show UPS: # FPS: #
         _window->display();
     }
 }
--- src/game_state.hpp
+++ src/game_state.hpp
@@ -20,14 +20,26 @@ namespace kg {
             void render(float dt);
 
         private:
+            // Updates run at 30 per second.
+            const float dt = 1.0f / 30.0f;
+
             WindowRef _window;
             StateMachineRef _machine;
+
+            sf::Clock _clock;
 
             sf::Texture _texture;
             sf::Sprite _sprite;
 
             sf::Font _font;
             sf::Text _text;
+            sf::Text _stats;
+
+            sf::Clock _upsClock;
+            sf::Clock _fpsClock;
+
+            int _updates, _ups;
+            int _frames, _fps;
 
             void init();
             void mainLoop();
